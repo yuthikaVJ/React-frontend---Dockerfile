0fcc87b9221ba927335d69d9fa8789c0c7a38b5b
Yuthika <yuthikavj@gmail.com>
2025-11-26 10:47:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+      <div className='app-container'>
+        <div className='card'>
+          <h1>Docker file sample</h1>
+          <p>This is a frontend page </p>
+          <p>hello </p>
+
+        </div>
+      </div>
+  )
+}
+
+export default App
